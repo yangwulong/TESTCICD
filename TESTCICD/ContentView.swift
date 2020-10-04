4a4ba528fc62db93e1b6af55d2db2c14c141e26a
Wulong yang <yange@uniware.com.au>
2020-10-05 10:39:57 +1100

    Initial commit

---
+++ TESTCICD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TESTCICD
+//
+//  Created by yang wulong on 5/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
